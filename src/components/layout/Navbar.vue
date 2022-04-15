323cbe4f83946de71344751680da8f1ea2fdea07
blsthathsara <blsthathsara@gmail.com>
2022-04-15 23:24:31 +0530

    Final Styles Updated Signed-off-by: blsthathsara <blsthathsara@gmail.com>

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -6,8 +6,9 @@
                 <ul class="right">
                     <li v-if="!user"><router-link :to=" {name : 'Signup'}">Signup</router-link></li>
                     <li v-if="!user"><router-link :to=" {name : 'Login'}">Login</router-link></li>
-                    <li v-if="user"><a>{{user.email}}</a></li>
+                    <li v-if="user" id="user-mail"><a>{{user.email}}</a></li>
                     <li v-if="user"><a @click="logout">Logout</a></li>
+                    <a id="user-mail-responsive" class="grey-text text-darken-2">{{user.email}}</a>
                 </ul>
             </div>
         </nav>
@@ -46,5 +47,19 @@ import firebase from 'firebase/compat/app'
 </script>
 
 <style>
-
+    @media screen and (max-width: 760px) {
+    #user-mail a{
+        display: none;
+    }
+    #user-mail-responsive{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+}
+    @media screen and (min-width: 760px) {
+    #user-mail-responsive{
+        display: none;
+    }
+}
 </style>
